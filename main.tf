742b454243145f68c9900ac60b4a312158854e05
GridIland <koussougboss@gmail.com>
2026-01-15 12:05:25 +0000

    Correction du provider

--- main.tf
+++ main.tf
@@ -7,23 +7,23 @@ terraform {
   }
 }
 
-provider "docker" {}
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
 
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
   keep_locally = false
 }
 
-# La magie opère ici avec "count"
 resource "docker_container" "app_server" {
-  count = var.server_count # Crée autant de ressources que le nombre défini
+  count = var.server_count 
   
   image = docker_image.nginx.image_id
-  name  = "server-web-${count.index + 1}" # Ex: server-web-1, server-web-2
+  name  = "server-web-${count.index + 1}"
 
   ports {
     internal = 80
-    # Chaque serveur aura un port unique : 8080, 8081, 8082...
     external = var.base_port + count.index 
   }
 }
